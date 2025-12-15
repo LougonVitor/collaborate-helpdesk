419cb1a0ad36511b122a862ccfb56fca0d082401
Gabriel campos <gabrielfigueira88@gmail.com>
2025-12-15 19:25:18 -0300

    feat: add filter in table of tickets

--- HelpDesk/obj/Debug/net9.0/HelpDesk.AssemblyInfo.cs
+++ HelpDesk/obj/Debug/net9.0/HelpDesk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d99fa7c6ffc0a7afdff01394483c4f286e2df09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6465123ed8341560c41eef10031658bd9c722eb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HelpDesk/wwwroot/js/site.js
+++ HelpDesk/wwwroot/js/site.js
@@ -3,14 +3,27 @@
 
 // Write your JavaScript code.
 
-(() => {
-    'use strict'
-    const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.forEach(tooltipTriggerEl => {
-        new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-})()
+    $("#chamados").DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ at&eacute; _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 at&eacute; 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+          
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
 
-$('.btn-close').click(function () {
-    $(".toast-shor").hide("hide")
-})
